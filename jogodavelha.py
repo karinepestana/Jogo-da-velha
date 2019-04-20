84e4633623c6abbb531cc2dadb82bf7ba169d9b8
Karine <karinepestana>
2019-04-20 11:08:04 -0300

    funcao de avalicao de estado final feita

--- jogodavelha.py
+++ jogodavelha.py
@@ -16,16 +16,33 @@ def expande():
 	for i in range(0,size):
 		for j in range(0,size):
 			aux = copy.deepcopy(tabuleiro)
-			if aux[i][j] == 0:
-				aux[i][j] = "x"
+			if aux[i][j] == '0':
+				aux[i][j] = 'x'
 				lista.append(aux)
 				aux = []
 	return lista
 
-tabuleiro = []
+def estado final(estado):
+	if estado[0][0] == estado[0][1] == estado[0][2]:
+		return True, estado[0][0]
+	elif estado[1][0] == estado[1][1] == estado[1][2]:
+		return True, estado[1][0]
+	elif estado[2][0] == estado[2][1] == estado[2][2]:
+		return True, estado[1][0]
+	elif estado[0][0] == estado[1][0] == estado[2][0]:
+		return True, estado[0][0]
+	elif estado[0][1] == estado[1][1] == estado[2][1]:
+		return True, estado[0][1]
+	elif estado[0][2] == estado[1][2] == estado[2][2]:
+		return True, estado[0][2]
+	elif estado[0][0] == estado[1][1] == estado[2][2]:
+		return True, estado[0][0]
+	elif estado[0][2] == estado[1][1] == estado[2][0]:
+		return True, estado[0][2]
+	else return False, '0'
+
 size = 3 
-for i in range(size):
-	tabuleiro.append([0]*size)
+tabuleiro = np.array(['0', '0', '0', '0', '0', '0', '0', '0', '0']).reshape(size,size)
 
 #while False:
 print("Estado atual do tabuleiro:\n")
@@ -34,7 +51,7 @@ print("Entre com a posição que deseja jogar")
 jogador2L = int(input("L:"))
 jogador2C = int(input("C:"))
 
-tabuleiro[jogador2L][jogador2C] = "o"
+tabuleiro[jogador2L][jogador2C] = 'o'
 saidas = expande()
 
 print(saidas)
